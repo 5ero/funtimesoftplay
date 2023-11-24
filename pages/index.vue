0d8d1e5469f3d8adf93b2c223efaab72ff120c50
Gareth <gareth71@gmail.com>
2023-11-24 16:18:17 +0000

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div>
+  <div class="flex flex-col items-center p-12">
+    <div class="mb-4">
+      <p class="text-4xl">
+        Coming soon...
+      </p>
+    </div>
+    <div class="mt-4">
+      <img src="/img/logo/funtime-softplay-logo.png" alt="" class="w-96">
+    </div>
+    <div class="flex flex-col items-center top-72 prose max-w-5xl p-8">
+      <p class="text-xl w-4/5 text-slate-600">
+        Soft play areas for kids offer a vibrant and engaging environment designed to ignite the imagination and energy of young ones. These playgrounds are a haven of padded structures, ball pits, and bouncy castles, all carefully crafted to encourage exploration, social interaction, and physical activity.
+      </p>
+    </div>
+    <div
+        class="w-full h-[550px]  bg-bottom bg-cover rounded-lg"
+        style="background-image:url('/img/displays/PhotoRoom_20231123_140823.jpeg');">
+    </div>
+    <div>
+      <p class="prose text-slate-600 mt-4">
+        For further information please contact us on <a href="tel:07942386386" class="underline">07942 386386</a> or email us at hello@funtimesoftplay.co.uk
+      </p>
+    </div>
+
+  </div>
+</div>
+</template>
+
+<style scoped>
+p {
+  font-family: 'Quicksand', sans-serif;
+}
+</style>
