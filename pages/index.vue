e7054c360d38fa2324c62edf183c2eea68c2d568
Gareth <gareth71@gmail.com>
2023-11-24 16:23:20 +0000

    Initial commit

--- pages/index.vue
+++ pages/index.vue
@@ -13,13 +13,13 @@
     <div class="mt-4">
       <img src="/img/logo/funtime-softplay-logo.png" alt="" class="w-96">
     </div>
-    <div class="flex flex-col items-center top-72 prose max-w-5xl p-8">
-      <p class="text-xl w-4/5 text-slate-600">
+    <div class="flex flex-col items-center mt-8 prose max-w-5xl md:p-8">
+      <p class="md:text-xl md:w-4/5 text-slate-600">
         Soft play areas for kids offer a vibrant and engaging environment designed to ignite the imagination and energy of young ones. These playgrounds are a haven of padded structures, ball pits, and bouncy castles, all carefully crafted to encourage exploration, social interaction, and physical activity.
       </p>
     </div>
     <div
-        class="w-full h-[550px]  bg-bottom bg-cover rounded-lg"
+        class="w-[320px] h-[215px] md:w-[900px] md:h-[550px]  bg-bottom bg-cover rounded-lg"
         style="background-image:url('/img/displays/PhotoRoom_20231123_140823.jpeg');">
     </div>
     <div>
