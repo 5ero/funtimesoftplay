2cc2fcd6fc2848206b7eb06d9d8c5c8c54b4e817
Gareth <gareth71@gmail.com>
2023-11-30 16:46:49 +0000

    updated sets

--- components/sets.vue
+++ components/sets.vue
@@ -52,6 +52,50 @@
       </p>
     </div>
   </div>
+  <div class="min-h-screen bg-slate-100">
+    <div class="max-w-7xl mx-auto md:p-8">
+      <div class="flex flex-col items-center py-12 md:pt-20">
+        <h1 class="text-5xl text-slate-600 md:py-2 font-semibold mb-6 text-center">
+          Luxury White "Maxi" Package
+        </h1>
+        <p class="p-8 prose py-2 text-slate-700 text-lg">
+          The entire soft play area is surrounded by a whimsical picket fence, adding a touch of charm and safety to the playground. Parents can watch with ease as their little ones engage in imaginative play within this captivating and secure environment.
+        </p>
+        <p class="p-8 prose py-2 text-slate-700 text-lg">
+          With its blend of adventure, creativity, and safety, our soft play setup invites children to embark on a delightful journey filled with endless fun and exploration. It's a place where imagination reigns supreme and every moment is an opportunity for magical memories to be created.
+
+        </p>
+        <div class="flex flex-col md:flex-row">
+          <div class="border border-slate-200 rounded m-4 bg-white">
+            <img src="/img/displays/maxi.jpg" class="p-2 md:p-4" alt="">
+          </div>
+          <div class="flex flex-wrap items-center justify-center w-full mx-auto m-4">
+            <icon src="/img/icons/blocks.png" label="blocks" />
+            <icon src="/img/icons/rocker.png" label="rocker" />
+            <icon src="/img/icons/rocker.png" label="rocker" />
+            <icon src="/img/icons/ballpit.png" label="ball pit" />
+            <icon src="/img/icons/doublerocker.png" label="Double Rocker" />
+            <icon src="/img/icons/maxistep.png" label="maxi step" />
+            <icon src="/img/icons/slide.png" label="slide" />
+            <icon src="/img/icons/rideonanimal.png" label="animal hopper" />
+            <icon src="/img/icons/rideonanimal.png" label="animal hopper" />
+            <icon src="/img/icons/rideonanimal.png" label="animal hopper" />
+            <icon src="/img/icons/rideonanimal.png" label="animal hopper" />
+            <icon src="/img/icons/maxistep.png" label="maxi step" />
+            <icon src="/img/icons/bridge.png" label="bridge" />
+            <icon src="/img/icons/slide.png" label="slide" />
+          </div>
+        </div>
+      </div>
+      <p class="text-slate-700 text-lg m-6">Complete with a variety of soft play equipment, this package is perfect for children aged 0-5 years old. It's a great choice for birthday parties, weddings, christenings, and other special occasions.</p>
+      <p class="py-6 text-slate-700 text-2xl m-6">
+        £295.00 <br>
+        <span class="text-sm">Add a picket fence for £60.00</span>
+        <icon src="/img/icons/picketfence.png" label="picket fence" />
+        <span class="text-sm text-gray-400">Note Delivery/Collection not included</span>
+      </p>
+    </div>
+  </div>
 </div>
 </template>
 
